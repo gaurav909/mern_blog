853d17ea3ffafa7e1ab263a32228c9c8161383fd
gaurav909 <gaurav150702@gmail.com>
2024-02-01 22:31:24 +0530

    make dash board private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,34 @@
-import React from 'react'
+import React from "react";
 
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import Dashboard from './pages/Dashboard'
-import Project from './pages/Project'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Project from "./pages/Project";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/sign-in" element={<SignIn/>}/>
-      <Route path="/sign-up" element={<SignUp/>}/>
-      <Route path="/dashboard" element={<Dashboard/>}/>
-      <Route path="/project" element={<Project/>}/>
-    </Routes>
-    <Footer/>
-    </BrowserRouter>
-  )
-}
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
 
-export default App
+        <Route path="/project" element={<Project />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
+
+export default App;
