fc19de4ab6616d74a3d3ddca3da126ef8d31e21d
gaurav909 <gaurav150702@gmail.com>
2024-02-13 23:40:39 +0530

    complete user image upload functionality

--- client/src/components/DashSideBar.jsx
+++ client/src/components/DashSideBar.jsx
@@ -1,30 +1,34 @@
 import { Sidebar } from "flowbite-react";
 import React, { useEffect, useState } from "react";
-import {HiUser,HiArrowSmRight} from 'react-icons/hi'
+import { HiUser, HiArrowSmRight } from "react-icons/hi";
 import { Link, useLocation } from "react-router-dom";
 
 const DashSideBar = () => {
-    const location = useLocation()
-  const [tab,setTab]=useState('')
-  useEffect(()=>{
-    const urlParams = new URLSearchParams(location.search)
-    const tabFromUrl =urlParams.get('tab')
-    if(tabFromUrl){
-      setTab(tabFromUrl)
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
     }
-
-
-  },[location.search])
+  }, [location.search]);
   return (
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
         <Sidebar.ItemGroup>
-            <Link to='/dashboard?tab=profile'>
-          <Sidebar.Item active={tab==='profile'} icon={HiUser} label={"User"} labelColor="dark">
-            Profile
-          </Sidebar.Item>
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              label={"User"}
+              labelColor="dark"
+              as='div'
+            >
+              Profile
+            </Sidebar.Item>
           </Link>
-          <Sidebar.Item active icon={HiArrowSmRight}  className='cursor-pointer' >
+          <Sidebar.Item active icon={HiArrowSmRight} className="cursor-pointer">
             Sign Out
           </Sidebar.Item>
         </Sidebar.ItemGroup>
